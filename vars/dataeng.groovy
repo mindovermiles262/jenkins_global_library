e52c398eb7a8c43786341aa96f211f3435a20c32
Andy D <mindovermiles262@gmail>
2020-02-12 10:29:31 -0800

    figuring this out

--- vars/dataeng.groovy
+++ vars/dataeng.groovy
@@ -3,8 +3,7 @@
 def configGlobal(String envName = 'test') {
   def envFile = libraryResource 'global-env-config.yml'
   def config = readYaml text: envFile
-  echo "Config:"
-  echo config.environments.test
-  echo "END"
+  def thisEnv = config.environments.${envName}
+  echo thisEnv.var1
 }
 
