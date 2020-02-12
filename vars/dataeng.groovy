d01d3ecc83c51454f6435c349c4cb5fc418e4614
Andy D <mindovermiles262@gmail>
2020-02-12 11:16:37 -0800

    some changes

--- vars/dataeng.groovy
+++ vars/dataeng.groovy
@@ -3,6 +3,7 @@
 def configGlobal(String envName = 'test', String yamlFile = 'global-env-config.yml') {
   def envFile = libraryResource yamlFile
   def config = readYaml text: envFile
+  echo "envName: ${envName}"
   config['environments'][envName].each{
     println "$it.key = $it.value" 
   }
