89689dfd0055ab27169557c363569c9d7d87467d
Andy D <mindovermiles262@gmail>
2020-02-12 09:57:13 -0800

    updated resource file name in dataeng groovy file

--- vars/dataeng.groovy
+++ vars/dataeng.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def configGlobal(String envName = 'test') {
-  def envFile = libraryResource 'env-branch.yml'
+  def envFile = libraryResource 'global-env-config.yml'
   def config = readYaml text: envFile
   echo "Config:"
   echo config
