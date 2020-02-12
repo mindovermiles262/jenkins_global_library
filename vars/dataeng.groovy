d572d6cae6dcc565c644bcfa2c961269c9d4b13f
Andy D <mindovermiles262@gmail>
2020-02-12 13:48:04 -0800

    trying my luck with props

--- vars/dataeng.groovy
+++ vars/dataeng.groovy
@@ -1,10 +1,22 @@
 #!/usr/bin/env groovy
 
-def configGlobal(String envName = 'test', String yamlFile = 'global-env-config.yml') {
-  def envFile = libraryResource yamlFile
-  def config = readYaml text: envFile
-  config['environments'][envName].each{ 
-    sh "export ${it.key}='${it.value}'"
+def configGlobal(String envName = 'test', String yamlFile = 'dataeng-env.yml') {
+  // Use this method to set global ENV Variables
+
+  // def envFile = libraryResource yamlFile
+  // def config = readYaml text: envFile
+  // config['environments'][envName].each{ 
+  //   sh "export ${it.key}='${it.value}'"
+  // }
+}
+
+def configLocal(String envName = 'test', String yamlFile = 'dataeng-props.yml') {
+  def propsFile = libraryResource yamlFile // Looks in resources directory
+  def config = readYaml text: propsFile
+  def props = [:]
+  config['environments'][envName].each {
+    sh "echo ${it.key} is ${it.value}"
+    props[it.key] = it.value
   }
 }
 
